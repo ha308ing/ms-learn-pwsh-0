50722142ba66efaf15d8fbb6a3eb6ba4c974d7ed
John Doe <75647984+ha308ing@users.noreply.github.com>
2022-05-28 01:12:25 +0300

    Backup. Error handling

--- Backup.ps1
+++ Backup.ps1
@@ -1,7 +1,23 @@
 Param (
   [string]$Path = '.\app',
-  [string]$DestinationPath = '.\'
+  [string]$DestinationPath = '.\',
+  [switch]$PathIsWebApp
 )
+# check content if $PathIsWebApp
+If ( $PathIsWebApp ) {
+  Try {
+    $ContainsApplicationFiles = `
+      "$( (Get-ChildItem $Path).Extension | Sort-Object -Unique )" `
+      -match '\.js|\.html|\.css'
+    If ( -Not $ContainsApplicationFiles ) {
+      Throw "Not a web app"
+    } Else {
+      Write-Host "Source files look good, continuing"
+    }
+  } Catch {
+    Throw "No backup created due to: $( $_.Exception.Message)"
+  }
+}
 # add check for $Path parameter
 If ( -Not ( Test-Path $Path ) ) {
   Throw "The source directory $Path does not exist, please specify an existing directory"
