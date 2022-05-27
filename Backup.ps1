fc6ee4441557cc078f180c6d359d5349248d163d
John Doe <75647984+ha308ing@users.noreply.github.com>
2022-05-27 23:40:40 +0300

    Backup. Input sanitation, flow control

--- Backup.ps1
+++ Backup.ps1
@@ -2,12 +2,21 @@ Param (
   [string]$Path = '.\app',
   [string]$DestinationPath = '.\'
 )
+# add check for $Path parameter
+If ( -Not ( Test-Path $Path ) ) {
+  Throw "The source directory $Path does not exist, please specify an existing directory"
+}
 $date = Get-Date -format "yyyy-MM-dd"
 $DestinationPath = `
   $($DestinationPath + 'backup-' + $date + '.zip')
-Compress-Archive `
-  -Path $Path `
-  -DestinationPath $DestinationPath `
-  -CompressionLevel Fastest
-Write-Host `
-  "Created backup at $DestinationPath"
+# add destination check
+If ( -Not ( Test-Path $DestinationPath ) ) {
+  Compress-Archive `
+    -Path $Path `
+    -DestinationPath $DestinationPath `
+    -CompressionLevel Fastest
+  Write-Host `
+    "Created backup at $DestinationPath"
+} Else {
+  Write-Error "Today's backup already exists"
+}
